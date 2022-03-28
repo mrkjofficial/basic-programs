8ed701095b73c4966f7ec70d05eb8d29419227bc
Karan Jaiswal <karanjaiswal0000@gmail.com>
2022-03-29 00:29:16 +0530

    Commit @ 29-03-2022

---
+++ CPP/CPP115.cpp
@@ -0,0 +1,31 @@
+/* 115. Program to count vowels in a given string */
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int countVowels(string);
+int main()
+{
+    string str;
+    cout << endl;
+    cout << "Enter a String: ";
+    getline(cin, str);
+    cin.sync();
+    cout << endl;
+    cout << "Number of Vowels: " << countVowels(str) << endl;
+}
+
+int countVowels(string str)
+{
+    int count = 0;
+    transform(str.begin(), str.end(), str.begin(), ::toupper);
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+        {
+            count++;
+        }
+    }
+    return count;
+}
